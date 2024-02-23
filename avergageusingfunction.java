079ec3926cdff070683ff0bdee1eb1f7f7bed20b
yashwanth <yashwanthnaddana2002@gmail.com>
2024-02-23 15:24:37 +0530

    This is only for dsa

--- avergageusingfunction.java
+++
@@ -1,8 +0,0 @@
-public class avergageusingfunction {
-    public static float avg(float a, float b, float c) {
-        return (a + b + c) / 3;
-    }
-    public static void main(String[] args) {
-        System.out.println(avg(2.3f , 2.4f , 5.6f));
-    }
-}
--- mathclass.java
+++
@@ -1,8 +0,0 @@
-public class mathclass {
-    public static void main(String[] args) {
-        System.out.println(Math.E);
-        System.out.println((Math.round(Math.PI)));
-    }
-
-    
-}
