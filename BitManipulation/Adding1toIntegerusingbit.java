cb98e1b6d55f831dd944c2aceb83b905a74d980b
yashwanth <yashwanthnaddana2002@gmail.com>
2024-03-19 16:30:39 +0530

    Added Adding 1 to integer using bitmanipulation

---
+++ BitManipulation/Adding1toIntegerusingbit.java
@@ -0,0 +1,8 @@
+package BitManipulation;
+
+public class Adding1toIntegerusingbit {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x + " + 1 is " + -~x);
+    }
+}
